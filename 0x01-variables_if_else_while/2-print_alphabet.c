9d7ea69f286a2a7c53254d99fb7228184ba861a8
Andrea Angola <3469@holbertonschool.com>
2021-06-18 18:28:04 -0700

    print lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int lowercase = 97;
+while (lowercase <= 122)
+{
+putchar(lowercase);
+lowercase++;
+}
+return (0);
+}
