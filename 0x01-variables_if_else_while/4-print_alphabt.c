d2b254e499f9bcc9a307c7ceefcf064cf26a48b3
Andrea Angola <3469@holbertonschool.com>
2021-06-18 19:55:30 -0700

    alphabet without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int lowercase = 97;
+while (lowercase <= 122)
+{
+if (lowercase != 101 && lowercase != 113)
+{
+putchar(lowercase);
+}
+lowercase++;
+}
+putchar('\n');
+return (0);
+}
