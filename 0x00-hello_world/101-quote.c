de75abf3d058d6b53e57ac11fa0cddf3951d96da
Andrea Angola <3469@holbertonschool.com>
2021-06-20 17:00:18 -0700

    print array

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,65 +1,21 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	putchar('a');
-	putchar('n');
-	putchar('d');
-	putchar(32);
-	putchar('t');
-	putchar('h');
-	putchar('a');
-	putchar('t');
-	putchar(32);
-	putchar('p');
-	putchar('i');
-	putchar('e');
-	putchar('c');
-	putchar('e');
-	putchar(32);
-	putchar('o');
-	putchar('f');
-	putchar(32);
-	putchar('a');
-	putchar('r');
-	putchar('t');
-	putchar(32);
-	putchar('i');
-	putchar('s');
-	putchar(32);
-	putchar('u');
-	putchar('s');
-	putchar('e');
-	putchar('f');
-	putchar('u');
-	putchar('l');
-	putchar(34);
-	putchar(32);
-	putchar(45);
-	putchar(32);
-	putchar('D');
-	putchar('o');
-	putchar('r');
-	putchar('a');
-	putchar(32);
-	putchar('K');
-	putchar('o');
-	putchar('r');
-	putchar('p');
-	putchar('a');
-	putchar('r');
-	putchar(44);
-	putchar(32);
-	putchar('2');
-	putchar('0');
-	putchar('1');
-	putchar('5');
-	putchar(45);
-	putchar('1');
-	putchar('0');
-	putchar(45);
-	putchar('1');
-	putchar('9');
-	putchar('\n');
-	return (1);
+int contador = 0;
+
+char letters[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+while (contador <= 58)
+{
+	putchar(letters[contador]);
+	contador++;
+}
+putchar('\n');
+return (1);
 }
 
