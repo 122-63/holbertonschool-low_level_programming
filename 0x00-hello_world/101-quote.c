2ef399cf6f111564dbc8dc1dd75f438137988bde
Andrea Angola <3469@holbertonschool.com>
2021-06-20 17:02:34 -0700

    print array

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@ int main(void)
 int contador = 0;
 
 char letters[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-while (contador <= 58)
+while (contador <= 57)
 {
 	putchar(letters[contador]);
 	contador++;
