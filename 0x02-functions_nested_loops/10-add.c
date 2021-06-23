413f01ea9eb062c850553fa7446dc4e5025ab08f
Andrea Angola <3469@holbertonschool.com>
2021-06-22 21:32:02 -0700

    suma

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - suma
+ * @a: primer numero
+ * @b: segundo nuemro
+ *
+ * Return: int.
+ */
+int add(int a, int b)
+{
+	long int resultado;
+
+	resultado = a + b;
+	return (resultado);
+}
+
