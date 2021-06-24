f5574468b2ffb9a9c4df64c16c9fb3228dd35185
Andrea Angola <3469@holbertonschool.com>
2021-06-24 10:21:25 -0700

    mult de dos numeros

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - miltiplica dos numeros
+ * @num1: numero 1
+ * @num2: numero 2
+ *
+ * Return: resultado
+ */
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 int _isupper(int upper);
 int _isdigit(int digit);
+int mul(int num1, int num2);
+int _putchar(char c);
+void print_numbers(void);
 
 #endif
