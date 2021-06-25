e0a6ba20eaa4e15cc6fdcbde4e5b7f76115c490a
Andrea Angola <3469@holbertonschool.com>
2021-06-24 18:55:43 -0700

    diagonal de diagonales

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_diagonal - dibuja una diagonal en diagolar
+ * @n: valor que entra
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+	int cont = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	while (cont < n)
+	{
+		int cont2 = 0;
+			while (cont2 < cont)
+			{
+				_putchar(32);
+				cont2++;
+			}
+		_putchar(47);
+		_putchar('\n');
+		cont++;
+	}
+}
