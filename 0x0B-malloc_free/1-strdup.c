443700c513fee6101aa91000cf2e3c082a40a3c5
Andrea Angola <3469@holbertonschool.com>
2021-07-13 16:38:15 -0700

    returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * strdup - eturns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str:variable
+ * Return: Always n.
+ */
+char *_strdup(const char *str)
+{
+	char *n = malloc(strlen(str) + 1);
+
+	if (n == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(n, str);
+	return (n);
+}
