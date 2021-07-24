e5016ada59da9de8e313df95d3ea110115e90d86
Andrea Angola <3469@holbertonschool.com>
2021-07-23 17:11:21 -0700

    prints strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints strings
+ * @separator: puntero
+ * @n: variable
+ * Return: Always 0.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *s;
+	va_list string;
+
+	va_start(string, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(string, char*);
+
+		if (s == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", s);
+		}
+
+		if (separator != NULL && i != (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	putchar('\n');
+	va_end(string);
+}
