0f96b4ac236812b5b73a8d804aa9bc2520cd8726
Andrea Angola <3469@holbertonschool.com>
2021-08-03 18:45:13 -0700

    inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,6 +28,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			return (NULL);
 		}
+		if (idx == 0)
+		{
+			return (NULL);
+		}
 		new_nodo->n = n;
 		if (aux == NULL && idx == 0)
 		{
